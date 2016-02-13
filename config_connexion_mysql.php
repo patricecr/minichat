397b248a0c0c2880863e14a4a27d36e47ba69c82
pcrucifix <pcrucifix@gmail.com>
2016-02-13 10:16:33 +0100

    Ajout code php pour connexion à la db

---
+++ config_connexion_mysql.php
@@ -0,0 +1,6 @@
+<?php 
+  /* Introduisez ici vos paramètres s'ils sont différents */
+  $db_name = 'test';    // nom de la base de données
+  $db_login = 'root';   // votre login
+  $db_passwd = 'root';  // votre mot de passe
+ ?>
--- minichat.php
+++ minichat.php
@@ -1,24 +1,42 @@
 <?php
-	
+/* Inclure les paramètres de connexion à la base de données MySQL. */
+require('config_connexion_mysql.php');
+
+/* Connexion à la base de données MySQL en utilisant les paramètres définis 
+    dans le fichier de configuration config_connexion_mysql.php */
+try {
+    $db = new PDO('mysql:host=localhost;dbname='.$db_name.';charset=utf8', $db_login, $db_passwd);
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+
+/* Exécuter une requête afin de récupérer tous les messages existants */
+$sql = "SELECT pseudo, message, DATE_FORMAT(date_message, '%d/%m/%Y à %Hh %imin %ss') as date_msg FROM minichat ORDER BY date_message DESC";
+$query = $db->query($sql);
+if(!$query){
+    die('Erreur de chargement de la table minichat. Verifiez si la table a bien ete importee dans la base de donnees "'.$db_name.'".');
+}
+var_dump($query);
+
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8" />
-		<title>minichat</title>
-	</head>
-	<body>
-		<h1>Minichat</h1>
-		<form action="" method="post">
-			<fieldset>
-				<legend>Poster un message</legend>
-				<p><label for="pseudo">Pseudo :</label> <input type="text" name="pseudo" id="pseudo" maxlength="60" value="" /></p>
-				<p><label for="message">Message :</label> <input type="text" name="message" id="message" size="75" maxlength="200" /></p>
-				<p><label></label> <input type="submit" value="Poster" /></p>
-			</fieldset>
-		</form>
-		
-		<h2>Messages</h2>
-		<p>Récupérer les messages avec php</p>	
-	</body>
+    <head>
+        <meta charset="utf-8" />
+        <title>minichat</title>
+    </head>
+    <body>
+        <h1>Minichat</h1>
+        <form action="" method="post">
+            <fieldset>
+                <legend>Poster un message</legend>
+                <p><label for="pseudo">Pseudo :</label> <input type="text" name="pseudo" id="pseudo" maxlength="60" value="" /></p>
+                <p><label for="message">Message :</label> <input type="text" name="message" id="message" size="75" maxlength="200" /></p>
+                <p><label></label> <input type="submit" value="Poster" /></p>
+            </fieldset>
+        </form>
+
+        <h2>Messages</h2>
+        <p>Récupérer les messages avec php</p>	
+    </body>
 </html>
